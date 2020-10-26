09ee54b6b107b09ff89863e6c1324f0114b9403a
jej2660 <juj2660@naver.com>
2020-10-26 18:22:59 +0900

    Initial Commit

---
+++ TnsSocket/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TnsSocket
+//
+//  Created by 장재원 on 2020/10/26.
+//  Copyright © 2020 장재원. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
